773ea5e1d15cc8bf9c7f27fe7c6a98b13ec995ba
PedroFernandesG <147552248+PedroFernandesG@users.noreply.github.com>
2024-12-22 18:11:20 -0300

    Update algorithm.ino

--- Line Follower Trolley with Arduino/algorithm.ino
+++ Line Follower Trolley with Arduino/algorithm.ino
@@ -1,1 +1,27 @@
-s
+bool onLine = false;
+bool left = true;
+const int Delay = 180;
+const int DelayT = 10;
+unsigned int start = 0;
+
+void algorithm(){
+
+  setVelocity(0, 75);
+  setVelocity(1, 80);
+  onLine = false;
+  left = !left;
+
+  while (((millis() - start) < Delay) || stayOnBlackLine() || !onLine ) {
+    if(left){
+     turnLeft();
+    }
+    else{
+     turnRight();
+    }
+    if (stayOnBlackLine()){
+      onLine = true;
+      start = millis();
+    }
+    delay(DelayT);
+  }
+}
